7fc967acc8af92b5387f0d5f456da4bd4076fec7
凌 <myywlc@163.com>
2021-05-30 21:23:40 +0800

    feat: 用useAsync处理loading和error状态

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { SearchPanel } from 'screens/project-list/search-panel';
 import { List } from 'screens/project-list/list';
-import { cleanObject, useDebounce, useMount } from '../../utils';
-import { useHttp } from 'utils/http';
+import { useDebounce } from '../../utils';
 import styled from '@emotion/styled';
+import { Typography } from 'antd';
+import { useProjects } from 'utils/project';
+import { useUsers } from 'utils/user';
 
 const Container = styled.div`
   padding: 3.2rem;
@@ -12,30 +14,22 @@ const Container = styled.div`
 // 使用 JS 的同学，大部分的错误都是在 runtime(运行时) 的时候发现的
 // 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
 export const ProjectListScreen = () => {
-  const [users, setUsers] = useState([]);
-
   const [param, setParam] = useState({
     name: '',
     personId: '',
   });
   const debouncedParam = useDebounce(param, 200);
-  const [list, setList] = useState([]);
-  const client = useHttp();
-
-  useEffect(() => {
-    client('projects', { data: cleanObject(debouncedParam) }).then(setList);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [debouncedParam]);
-
-  useMount(() => {
-    client('users').then(setUsers);
-  });
+  const { isLoading, error, data: list } = useProjects(debouncedParam);
+  const { data: users } = useUsers();
 
   return (
     <Container>
       <h1>项目列表</h1>
-      <SearchPanel users={users} param={param} setParam={setParam} />
-      <List users={users} list={list} />
+      <SearchPanel users={users || []} param={param} setParam={setParam} />
+      {error ? (
+        <Typography.Text type={'danger'}>{error.message}</Typography.Text>
+      ) : null}
+      <List loading={isLoading} users={users || []} dataSource={list || []} />
     </Container>
   );
 };
