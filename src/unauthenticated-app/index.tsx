95b177279ff9deef45087e7f36afa92f4a768038
凌 <myywlc@163.com>
2021-05-30 22:19:38 +0800

    feat: 用 useDocumentTitle 改变文档标题

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -6,6 +6,7 @@ import styled from '@emotion/styled';
 import logo from 'assets/logo.svg';
 import left from 'assets/left.svg';
 import right from 'assets/right.svg';
+import { useDocumentTitle } from 'utils';
 
 export const LongButton = styled(Button)`
   width: 100%;
@@ -55,6 +56,8 @@ const Container = styled.div`
 export const UnauthenticatedApp = () => {
   const [isRegister, setIsRegister] = useState(false);
   const [error, setError] = useState<Error | null>(null);
+
+  useDocumentTitle('请登录注册以继续');
 
   return (
     <Container>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -51,3 +51,19 @@ export const useArray = <T>(initialArray: T[]) => {
     },
   };
 };
+
+export const useDocumentTitle = (title: string, keepOnUnmount = true) => {
+  const oldTitle = document.title;
+
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
+  useEffect(() => {
+    return () => {
+      if (!keepOnUnmount) {
+        document.title = oldTitle;
+      }
+    };
+  }, []);
+};
