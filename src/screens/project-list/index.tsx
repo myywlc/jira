bf4d333802de287522b617eb97c7c1a4a06e1527
凌 <myywlc@163.com>
2021-05-30 17:39:48 +0800

    style: 优化项目列表页面

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SearchPanel } from 'screens/project-list/search-panel';
 import { List } from 'screens/project-list/list';
-import { useEffect, useState } from 'react';
 import { cleanObject, useDebounce, useMount } from '../../utils';
 import { useHttp } from 'utils/http';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  padding: 3.2rem;
+`;
 
 // 使用 JS 的同学，大部分的错误都是在 runtime(运行时) 的时候发现的
 // 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
@@ -28,9 +32,10 @@ export const ProjectListScreen = () => {
   });
 
   return (
-    <div>
+    <Container>
+      <h1>项目列表</h1>
       <SearchPanel users={users} param={param} setParam={setParam} />
       <List users={users} list={list} />
-    </div>
+    </Container>
   );
 };
