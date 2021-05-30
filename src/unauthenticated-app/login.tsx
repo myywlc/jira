1080eadf517d16402754312e8abef1e90b81d7b2
凌 <myywlc@163.com>
2021-05-30 21:36:36 +0800

    feat: 登录注册loading和错误处理

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 import { useAuth } from 'context/auth-context';
 import { Form, Input } from 'antd';
 import { LongButton } from 'unauthenticated-app';
+import { useAsync } from 'utils/use-async';
 
-export const LoginScreen = () => {
+export const LoginScreen = ({
+  onError,
+}: {
+  onError: (error: Error) => void;
+}) => {
   const { login } = useAuth();
-
+  const { run, isLoading } = useAsync(undefined, { throwOnError: true });
   // HTMLFormElement extends Element
-  const handleSubmit = (value: { username: string; password: string }) => {
-    login(value);
+  const handleSubmit = async (values: {
+    username: string;
+    password: string;
+  }) => {
+    try {
+      await run(login(values));
+    } catch (e) {
+      onError(e);
+    }
   };
 
   return (
@@ -26,7 +38,7 @@ export const LoginScreen = () => {
         <Input placeholder={'密码'} type="password" id={'password'} />
       </Form.Item>
       <Form.Item>
-        <LongButton htmlType={'submit'} type={'primary'}>
+        <LongButton loading={isLoading} htmlType={'submit'} type={'primary'}>
           登录
         </LongButton>
       </Form.Item>
