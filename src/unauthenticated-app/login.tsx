8b21399d39ca44ef67e9a475fcca72de5be72c13
凌 <myywlc@163.com>
2021-05-30 16:22:06 +0800

    style: 安装emotion，优化登录注册界面, 用antd替代原组件

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,30 +1,35 @@
-import React, { FormEvent } from 'react';
+import React from 'react';
 import { useAuth } from 'context/auth-context';
+import { Form, Input } from 'antd';
+import { LongButton } from 'unauthenticated-app';
 
 export const LoginScreen = () => {
   const { login } = useAuth();
 
   // HTMLFormElement extends Element
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const username = (event.currentTarget.elements[0] as HTMLInputElement)
-      .value;
-    const password = (event.currentTarget.elements[1] as HTMLInputElement)
-      .value;
-    login({ username, password });
+  const handleSubmit = (value: { username: string; password: string }) => {
+    login(value);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">用户名</label>
-        <input type="text" id={'username'} />
-      </div>
-      <div>
-        <label htmlFor="password">密码</label>
-        <input type="password" id={'password'} />
-      </div>
-      <button type={'submit'}>登录</button>
-    </form>
+    <Form onFinish={handleSubmit}>
+      <Form.Item
+        name={'username'}
+        rules={[{ required: true, message: '请输入用户名' }]}
+      >
+        <Input placeholder={'用户名'} type="text" id={'username'} />
+      </Form.Item>
+      <Form.Item
+        name={'password'}
+        rules={[{ required: true, message: '请输入密码' }]}
+      >
+        <Input placeholder={'密码'} type="password" id={'password'} />
+      </Form.Item>
+      <Form.Item>
+        <LongButton htmlType={'submit'} type={'primary'}>
+          登录
+        </LongButton>
+      </Form.Item>
+    </Form>
   );
 };
