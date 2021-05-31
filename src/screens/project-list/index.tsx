c0f6ec95cf118d3559c064d1f3a64bbcc88e420b
jiangchengbiao <jiangchengbiao@aidigger.com>
2021-05-31 17:14:38 +0800

    feat: 用 user-select.tsx选择用户

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -6,7 +6,10 @@ import styled from '@emotion/styled';
 import { Typography } from 'antd';
 import { useProjects } from 'utils/project';
 import { useUsers } from 'utils/user';
-import { useUrlQueryParam } from 'utils/url';
+import { useProjectsSearchParams } from 'screens/project-list/util';
+
+// 基本类型，可以放到依赖里；组件状态，可以放到依赖里；非组件状态的对象，绝不可以放到依赖里
+// https://codesandbox.io/s/keen-wave-tlz9s?file=/src/App.js
 
 const Container = styled.div`
   padding: 3.2rem;
@@ -15,14 +18,10 @@ const Container = styled.div`
 // 使用 JS 的同学，大部分的错误都是在 runtime(运行时) 的时候发现的
 // 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
 export const ProjectListScreen = () => {
-  // 基本类型，可以放到依赖里；组件状态，可以放到依赖里；非组件状态的对象，绝不可以放到依赖里
-  // https://codesandbox.io/s/keen-wave-tlz9s?file=/src/App.js
-  const [param, setParam] = useUrlQueryParam(['name', 'personId']);
-  const debouncedParam = useDebounce(param, 200);
-  const { isLoading, error, data: list } = useProjects(debouncedParam);
-  const { data: users } = useUsers();
-
   useDocumentTitle('项目列表', false);
+  const [param, setParam] = useProjectsSearchParams();
+  const { isLoading, error, data: list } = useProjects(useDebounce(param, 200));
+  const { data: users } = useUsers();
 
   return (
     <Container>
