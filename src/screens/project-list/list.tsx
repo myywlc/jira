22e86d89716b968e77295422922e0625decb0ba1
凌 <myywlc@163.com>
2021-06-01 23:25:30 +0800

    feat: 用状态提升实现projectModal状态管理

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { User } from 'screens/project-list/search-panel';
-import { Table } from 'antd';
+import { Dropdown, Menu, Table } from 'antd';
 import dayjs from 'dayjs';
 import { TableProps } from 'antd/es/table';
 import { Link } from 'react-router-dom';
 import { Pin } from 'components/pin';
 import { useEditProject } from 'utils/project';
+import { ButtonNoPadding } from 'components/lib';
 
 export interface Project {
   id: number;
@@ -19,6 +20,7 @@ export interface Project {
 interface ListProps extends TableProps<Project> {
   users: User[];
   refresh?: () => void;
+  setProjectModalOpen: (isOpen: boolean) => void;
 }
 
 export const List = ({ users, ...props }: ListProps) => {
@@ -75,6 +77,28 @@ export const List = ({ users, ...props }: ListProps) => {
                   ? dayjs(project.created).format('YYYY-MM-DD')
                   : '无'}
               </span>
+            );
+          },
+        },
+        {
+          render(value, project) {
+            return (
+              <Dropdown
+                overlay={
+                  <Menu>
+                    <Menu.Item key={'edit'}>
+                      <ButtonNoPadding
+                        type={'link'}
+                        onClick={() => props.setProjectModalOpen(true)}
+                      >
+                        编辑
+                      </ButtonNoPadding>
+                    </Menu.Item>
+                  </Menu>
+                }
+              >
+                <ButtonNoPadding type={'link'}>...</ButtonNoPadding>
+              </Dropdown>
             );
           },
         },
