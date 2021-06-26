47682758e6c3e592c1706cff238314f0e7960e33
凌 <myywlc@163.com>
2021-06-27 02:09:03 +0800

    feat: 添加search tasks功能

--- src/screens/kanban/util.ts
+++ src/screens/kanban/util.ts
@@ -1,5 +1,7 @@
+import { useMemo } from 'react';
 import { useLocation } from 'react-router';
 import { useProject } from 'utils/project';
+import { useUrlQueryParam } from 'utils/url';
 
 export const useProjectIdInUrl = () => {
   const { pathname } = useLocation();
@@ -13,6 +15,19 @@ export const useKanbanSearchParams = () => ({ projectId: useProjectIdInUrl() });
 
 export const useKanbansQueryKey = () => ['kanbans', useKanbanSearchParams()];
 
-export const useTasksSearchParams = () => ({ projectId: useProjectIdInUrl() });
+export const useTasksSearchParams = () => {
+  const [param, setParam] = useUrlQueryParam(['name', 'typeId', 'processorId', 'tagId']);
+  const projectId = useProjectIdInUrl();
+  return useMemo(
+    () => ({
+      projectId,
+      typeId: Number(param.typeId) || undefined,
+      processorId: Number(param.processorId) || undefined,
+      tagId: Number(param.tagId) || undefined,
+      name: param.name,
+    }),
+    [projectId, param],
+  );
+};
 
 export const useTasksQueryKey = () => ['tasks', useTasksSearchParams()];
